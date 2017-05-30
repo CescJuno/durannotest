12ea053e226829d8d32a751c25aa54b8802ffac5
CescJuno <nallayo@nate.com>
2017-05-30 14:50:57 +0900

    npm update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var server = app.listen(3000, function(){
     console.log("Express server has started on port 3000")
+
 });
 
 app.get('/', function(req, res){
