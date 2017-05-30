6a05342aaea19bdee7284df848dee442746ceb54
CescJuno <nallayo@nate.com>
2017-05-30 15:22:17 +0900

    test

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var express = require('express');
-var app = express();
-var server = app.listen(3000, function(){
-    console.log("Express server has started on port 3000")
+var http=require('http');
 
+var server=http.createServer(function(req,res){
+    res.end('test');
 });
+
+server.on('listening',function(){
+    console.log('ok, server is running');
+});
+
+server.listen(80);
