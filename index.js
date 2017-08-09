97d920bdd07eb4a79cc75e07d3bdd1a710b8eff6
CescJuno <nallayo@nate.com>
2017-08-09 11:48:31 +0900

    asdfsdfsdf

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 var http = require('http');
+/*
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
@@ -20,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 
-
+*/
 //module.exports = app;
 
 var port = process.env.port || 1337;
