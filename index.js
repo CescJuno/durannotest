e179c7a71ea3eff580e6a82f1a39cdcb594dde98
CescJuno <nallayo@nate.com>
2017-08-09 11:46:43 +0900

    cccc

--- index.js
+++ index.js
@@ -25,8 +25,6 @@ app.use('/', index);
 //module.exports = app;
 
 var port = process.env.port || 1337;
-//http.createServer(app).listen(port);
-
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World!!!!!!\n');
