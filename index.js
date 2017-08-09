ff5930f3bdee558fe6ca7977551e176b446cfe11
CescJuno <nallayo@nate.com>
2017-08-09 11:37:44 +0900

    11

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 
 var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
