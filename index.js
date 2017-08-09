8f06dedc30ff6ecf7c5fdf6e36c2b8b9f4470945
CescJuno <nallayo@nate.com>
2017-08-09 11:36:20 +0900

    aaaa

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 var http = require('http');
 var express = require('express');
 var index = require('./routes/index');
+
+var app = express();
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
