1a056103f08cf467b9a79c82d1e927cc0e7a0489
CescJuno <nallayo@nate.com>
2017-08-09 11:38:29 +0900

    aaaa

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
